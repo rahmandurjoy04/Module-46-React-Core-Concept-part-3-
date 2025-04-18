db71831a98349a2f1e19fe96c18f21d9ef88fa89
rahmandurjoy04 <nainurrahman70@gmail.com>
2025-04-18 21:30:17 +0600

    Final Touches Added

--- src/Components/FamilyTree/Me/Brother/Brother.jsx
+++ src/Components/FamilyTree/Me/Brother/Brother.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { use } from 'react';
+import { MoneyContext } from '../../FamilyTree';
 
 const Brother = () => {
+    const [money,setMoney]=use(MoneyContext);
     return (
         <div className='border rounded p-1 m-1 bg-indigo-400'>
             <h2>Brother</h2>
+            <button onClick={()=>setMoney(money+1000)} className='btn btn-xs'>Add $1000</button>
         </div>
     );
 };
